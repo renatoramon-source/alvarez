63c11cfe60d5725fd9b53863a368c35a7ada3a1a
renatoramon-source <renatoramon929@gmail.com>
2026-03-30 20:25:12 -0300

    finalizando atividade

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function mudar_conteudo_elemento() {
+function mudar_conteudo_elemento(){
 
-  document.getElementById("h1").innerHTML = "Hello JavaScript!";
+  document.getElementById("h1").innerHTML = "Hello Javascript!";
+
+
+}
+
+function mudar_conteudo_elemento(){
+
+  document.getElementById("p").innerHTML = "Minha função de mudar o conteudo funcionou!";
+
+
 }
